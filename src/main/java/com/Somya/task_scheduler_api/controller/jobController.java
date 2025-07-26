e300bc1da5ed3daaee02760748af9fba1958ffdd
somya3819 <somyamahajan597@gmail.com>
2025-07-26 19:46:51 +0530

    commit 1

---
+++ src/main/java/com/Somya/task_scheduler_api/controller/jobController.java
@@ -0,0 +1,45 @@
+// src/main/java/com/Somya/task_scheduler_api/controller/JobController.java
+package com.Somya.task_scheduler_api.controller;
+
+import com.Somya.task_scheduler_api.model.Job;
+import com.Somya.task_scheduler_api.repository.jobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController // Tells Spring this class will handle web requests
+@RequestMapping("/api/jobs") // All endpoints in this class will start with /api/jobs
+public class jobController {
+
+    // Spring's magic to give us an instance of our JobRepository
+    @Autowired
+    private jobRepository jobRepository;
+
+    /**
+     * Endpoint to create a new job.
+     * Listens for POST requests to http://localhost:8080/api/jobs
+     */
+    @PostMapping
+    public ResponseEntity<Job> createJob(@RequestBody Job newJob) {
+        // The @PrePersist methods in your Job entity will set the timestamps automatically
+        Job savedJob = jobRepository.save(newJob);
+        return ResponseEntity.ok(savedJob);
+    }
+
+    /**
+     * Endpoint to get a job by its ID.
+     * Listens for GET requests to http://localhost:8080/api/jobs/{id}
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+
+        if (jobOptional.isPresent()) {
+            return ResponseEntity.ok(jobOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
